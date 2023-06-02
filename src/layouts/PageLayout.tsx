2f4708421f40fd7aa7679f9d8c2ac26cd56df929
Ozan Honamlioglu <ozanhonamlioglu@gmail.com>
2023-06-02 18:38:31 +0300

    page refactor

--- src/layouts/PageLayout.tsx
+++ src/layouts/PageLayout.tsx
@@ -1,23 +1,14 @@
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Container, { ContainerProps } from '@mui/material/Container';
-import Switch from '@mui/material/Switch';
 import Toolbar from '@mui/material/Toolbar';
 import React, { PropsWithChildren } from 'react';
 
+import DarkLightSwitch from 'components/DarkLightSwitch';
 import GrowingBorderMenu from 'components/menu/GrowingBorderMenu';
-import { useThemeContext, useThemeDispatchContext } from 'hooks/useThemeContext';
 
 const pages = ['Products', 'Pricing', 'Blog'];
 const PageLayout: React.FC<PropsWithChildren<ContainerProps>> = ({ children, ...props }) => {
-  const dispatch = useThemeDispatchContext();
-  const theme = useThemeContext();
-
-  const handleSwitch = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const checked = event.target.checked;
-    dispatch?.({ type: 'changeThemeMode', payload: checked ? 'dark' : 'light' });
-  };
-
   return (
     <>
       <Box sx={{ flexGrow: 1 }}>
@@ -25,7 +16,7 @@ const PageLayout: React.FC<PropsWithChildren<ContainerProps>> = ({ children, ...
           <Toolbar>
             <GrowingBorderMenu pages={pages} />
             <Box sx={{ flex: 1 }} />
-            <Switch onChange={handleSwitch} checked={theme?.mode === 'dark'} color="secondary" />
+            <DarkLightSwitch />
           </Toolbar>
         </AppBar>
       </Box>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,16 +5,12 @@ import React from 'react';
 import PageLayout from 'layouts/PageLayout';
 
 import { BorderGrowingButton, BorderSwitchingButton, RotatedBackgroundButton } from 'components/buttons';
-import DarkLightSwitch from 'components/DarkLightSwitch';
 
 const Home = () => {
   const theme = useTheme();
   return (
     <PageLayout sx={{ padding: 10 }}>
       <Grid container spacing={10}>
-        <Grid item xs={12}>
-          <DarkLightSwitch />
-        </Grid>
         <Grid item xs={12}>
           <BorderGrowingButton disableFocusRipple>Forgot passworsd</BorderGrowingButton>
         </Grid>
